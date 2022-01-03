4e0ba6fc2601877aebc3b22f21b7b5006a43b452
Alex Couture-Beil <alex@earthly.dev>
2022-01-02 19:28:14 -0800

    test.r was not exiting with exit code
    When RUnit fails, exit code should be returned
    
    Signed-off-by: Alex Couture-Beil <alex@mofo.ca>

--- test.r
+++ test.r
@@ -1,8 +1,25 @@
 #!/usr/bin/Rscript
+
+v <- function(...) cat(sprintf(...), sep='', file=stderr())
+
 install.packages( '/code/rjson', repos=NULL )
 library( rjson )
 
 library( RUnit )
 path <- system.file( "unittests", package="rjson" )
 test.suite <- defineTestSuite( "json unittests", dirs = path, testFileRegexp = "^test\\..*\\.[rR]$" )
-runTestSuite( test.suite, verbose = 100 )
+result <- getErrors(runTestSuite( test.suite, verbose = 100 ))
+
+exit_code <- 0
+if( result$nFail > 0 ) {
+	v("%d test(s) failed\n", result$nFail)
+	exit_code <- 1
+}
+if( result$nErr > 0 ) {
+	v("%d test(s) had errors\n", result$nErr)
+	exit_code <- 1
+}
+if( exit_code == 0 ) {
+	v("tests passed\n")
+}
+quit(status=exit_code)
